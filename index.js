0c72b34fe2261d1964ca3c9deeee44c31b83650a
Alan <alan@raspberrypi.(none)>
2013-07-16 04:02:39 +0000

    added farenheit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var HID = require('HID');
+var HID = require('node-hid');
 var readCommand=[0x01, 0x80, 0x33, 0x01, 0x00, 0x00, 0x00, 0x00];
 exports.readTemperatures=function(devices) {
 };
@@ -7,34 +7,39 @@ exports.getDevices=function() {
  var devices=HID.devices();
  var list=[];
  devices.forEach(function(item) {
-   if(item.product==="TEMPer1V1.2" && 
+   if(//item.product==="TEMPer1V1.4" && 
       item.vendorId===3141 && 
-      item.interface===1){  list.push(item.path);
+      item.interface===1){  
+        list.push(item.path);
   }
  });
  return list;
 }
 
 exports.readTemperature=function(path, callback, converter){
- if(!converter) {
-  converter=exports.toDegreeCelcius;
- }
- var device = new HID.HID(path);
- device.write(readCommand);
- device.read(function(err,response){
-   if(err) {
-    callback.call(this,err,null); 
-   } else {
-    callback.call(this,null, converter(response[2],response[3]));
-   }
- });
+  if(!converter) {
+    converter=exports.toDegreeCelcius;
+  }
+  var device = new HID.HID(path);
+  device.write(readCommand);
+  device.read(function(err,response){
+    if(err) {
+      callback.call(this,err,null); 
+    } else {
+      callback.call(this,null, converter(response[2],response[3]));
+    }
+  });
 }
 
 exports.toDegreeCelcius=function(hiByte, loByte) {
- var sign = hiByte & (1 << 7);
- var temp = ((hiByte & 0x7F) << 8) | loByte;
- if (sign) {
+  var sign = hiByte & (1 << 7);
+  var temp = ((hiByte & 0x7F) << 8) | loByte;
+  if (sign) {
     temp = -temp;
- }
- return temp * 125.0 / 32000.0;
+  }
+  return temp * 125.0 / 32000.0;
+}
+
+exports.toDegreeFarenheit=function(celsius){
+  return celsius * 9/5 + 32;
 }
--- test/test.js
+++ test/test.js
@@ -6,4 +6,7 @@ console.log("Devices found:"+devices[0]);
 
 thermometers.readTemperature(devices[0], function(err, value) {
   console.log("Result:"+value);
+  console.log("Error:"+err);
+  var f = thermometers.toDegreeFarenheit(value);
+  console.log("F:"+f);
 });
