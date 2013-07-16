0cbb263a1f3286e81ff66631c0810f430d76e72b
Alan Harwood <malandro95@yahoo.com>
2013-07-15 22:51:13 -0600

    added json formatting

--- test/run_json.js
+++ test/run_json.js
@@ -2,8 +2,8 @@ thermometers=require("..");
 var fs = require('fs');
 
 var outputData = {
-  name:'test',
-  version:'1.0'
+  date:'null',
+  degreesF:0
 }
 
 var outputFilename = 'current_temp.json'; 
@@ -15,6 +15,13 @@ thermometers.readTemperature(devices[0], function(err, value) {
   console.log("Error:"+err);
   var f = thermometers.toDegreeFarenheit(value);
   console.log("F:"+f);
+  
+  var now = new Date().toISOString().
+    replace(/T/, ' ').      // replace T with a space
+    replace(/\..+/, '');
+
+  outputData.degreesF = f;
+  outputData.date = now;
   fs.writeFile(outputFilename, JSON.stringify(outputData, null, 4), function(err) {
       if(err) {
         console.log(err);
